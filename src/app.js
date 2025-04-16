b3571b6294985ffea82a4d9c949aa5c021655299
Babloo Kumar <97720510+Babloo321@users.noreply.github.com>
2025-04-17 00:18:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app = express();
-const allowedOrigins = ["https://krishi-mitra-client.vercel.app/","http://localhost:5173"];
+const allowedOrigins = ["https://krishi-mitra-client.vercel.app/","http://localhost:5173"]
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
